53c23458decaea0d1d28f759d54fca07c86033cc
Vadim Kuchinskiy <kuchinskiy.v@yandex.ru>
2026-01-13 19:43:47 +0700

    Исправления для прохождения тестов

--- main-service/src/main/java/ru/practicum/main/config/JacksonConfig.java
+++ main-service/src/main/java/ru/practicum/main/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package ru.practicum.main.config;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +16,17 @@ public class JacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
         return builder -> {
-
             builder.simpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-            builder.modules(new JavaTimeModule());
+            builder.serializers(new LocalDateSerializer(
+                    java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
             builder.serializers(new LocalDateTimeSerializer(FORMATTER));
+
+            builder.deserializers(new LocalDateDeserializer(
+                    java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+            builder.deserializers(new LocalDateTimeDeserializer(FORMATTER));
 
             builder.featuresToDisable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         };
