a465538d6f2b9eedea2894a3fb6a87b559ac04e6
Vadim Kuchinskiy <kuchinskiy.v@yandex.ru>
2026-01-13 18:28:20 +0700

    Удалены аннотации @JsonFormat из всех DTO; Добавлена централизованная конфигурация Jackson; Убраны аннотации @Transactional с уровня классов; Добавлены явные @Transactional к методам изменения данных; Методы чтения помечены @Transactional(readOnly = true); Заменена обработка DataIntegrityViolationException на явную проверку; Применен паттерн findById().map(mapper).orElseThrow() в CategoryServiceImpl.

--- main-service/src/main/java/ru/practicum/main/event/model/UpdateEventUserRequest.java
+++ main-service/src/main/java/ru/practicum/main/event/model/UpdateEventUserRequest.java
@@ -1,5 +1,6 @@
 package ru.practicum.main.event.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -24,6 +25,7 @@ public class UpdateEventUserRequest implements UpdateEventRequest {
     @Size(min = 20, max = 7000)
     private String description;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
 
     @Valid
